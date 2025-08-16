e3ae8a5eb95df31c5626c9303ecec1a3239abccd
Suraj Singh <suraj@tmdc.io>
2025-08-16 23:17:19 +0530

    removed s3

--- modules/elastic_beanstalk/data.tf
+++ modules/elastic_beanstalk/data.tf
@@ -1,19 +1,3 @@
 # Data sources for current region and account
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
-
-# Create a zip file of the application
-data "archive_file" "app_zip" {
-  type        = "zip"
-  output_path = "${path.module}/application.zip"
-
-  source {
-    content  = file("${path.root}/app.py")
-    filename = "app.py"
-  }
-
-  source {
-    content  = file("${path.root}/requirements.txt")
-    filename = "requirements.txt"
-  }
-}
--- modules/elastic_beanstalk/main.tf
+++ modules/elastic_beanstalk/main.tf
@@ -13,7 +13,6 @@ resource "aws_elastic_beanstalk_application" "app" {
 resource "aws_elastic_beanstalk_environment" "env" {
   name                = "${var.application_name}-${var.environment}"
   application         = aws_elastic_beanstalk_application.app.name
-  version_label = aws_elastic_beanstalk_application_version.app_version.name
   solution_stack_name = var.solution_stack_name
 
   # Environment variables
@@ -168,21 +167,4 @@ resource "aws_elastic_beanstalk_environment" "env" {
     Name        = "${var.application_name}-${var.environment}"
     Environment = var.environment
   }
-}
-
-# Upload application package to S3
-resource "aws_s3_object" "app_zip" {
-  bucket = "elasticbeanstalk-${data.aws_region.current.name}-${data.aws_caller_identity.current.account_id}"
-  key    = "application.zip"
-  source = data.archive_file.app_zip.output_path
-  etag   = data.archive_file.app_zip.output_md5
-}
-
-# Create Beanstalk application version
-resource "aws_elastic_beanstalk_application_version" "app_version" {
-  application = aws_elastic_beanstalk_application.app.name
-  bucket      = aws_s3_object.app_zip.bucket
-  key         = aws_s3_object.app_zip.key
-  name        = "version-${timestamp()}"
-  description = "App version"
 }
