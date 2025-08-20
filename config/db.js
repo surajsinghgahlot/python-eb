6c75d41c60c84d4e43d2d5ad5e7f19c0f9163097
Suraj Singh <suraj@tmdc.io>
2025-08-20 19:24:30 +0530

    db

--- config/db.js
+++ config/db.js
@@ -20,26 +20,8 @@ const connectDB = async () => {
   try {
     mongoose.set({ strictQuery: false });
     // mongoose.set('debug', true);
-    
-    // Add proper connection options with timeouts and retries
-    const connectionOptions = {
-      serverSelectionTimeoutMS: 30000, // 30 seconds
-      socketTimeoutMS: 45000, // 45 seconds
-      bufferMaxEntries: 0,
-      bufferCommands: false,
-      maxPoolSize: 10,
-      minPoolSize: 1,
-      maxIdleTimeMS: 30000,
-      retryWrites: true,
-      retryReads: true,
-      w: 'majority'
-    };
-    
-    await mongoose.connect(URL, connectionOptions);
-    
-    // Wait for connection to be ready
-    await mongoose.connection.asPromise();
-    
+    await mongoose.connect(URL, {
+    });
     // const collectionsCursor = mongoose.connection.db.listCollections();
     // const collections = await collectionsCursor.toArray();
     // const filteredCollections = collections.filter(
@@ -67,8 +49,6 @@ const connectDB = async () => {
 
   } catch (err) {
     console.log("Failed to connect to MongoDB", err);
-    // Exit process if database connection fails
-    process.exit(1);
   }
 };
 
